c9fbf630a42ddf0ab3bd8e942fefdee8f9d43af4
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2013-06-22 15:41:17 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gitopenApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
