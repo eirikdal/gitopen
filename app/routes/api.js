e1c45b568259888cb2f6935d11dadc354be9e013
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2013-06-24 21:31:14 +0200

    asdfAS

--- app/routes/api.js
+++ app/routes/api.js
@@ -31,7 +31,7 @@ exports.findAllContestants = function (req, res) {
 };
 
 exports.newCommit = function(req, res) {
-    mongodb.update({name:"foo", score: 1}, function (resp) {
+    mongodb.update({name:"foo", score: 2}, function (resp) {
         app.notify('onContestantUpdated', resp);
         res.json(resp);
     });
